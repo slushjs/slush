459dc5fcffd8a254b417caa2ebe01e11ad0150aa
Charles Samborski <demurgos@demurgos.net>
2017-12-31 00:03:23 +0100

    Drop dependency on deprecated `gulp-util`
    Closes slushjs/slush#61

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,21 @@
 'use strict';
 
 var gulp = require('gulp');
-var log = require('gulp-util').log;
+var fancyLog = require('fancy-log');
 
 var jshint = require('gulp-jshint');
 
 var codeFiles = ['**/*.js', '!node_modules/**'];
 
 gulp.task('lint', function() {
-  log('Linting Files');
+  fancyLog('Linting Files');
   return gulp.src(codeFiles)
     .pipe(jshint('.jshintrc'))
     .pipe(jshint.reporter());
 });
 
 gulp.task('watch', function() {
-  log('Watching Files');
+  fancyLog('Watching Files');
   gulp.watch(codeFiles, ['lint']);
 });
 
